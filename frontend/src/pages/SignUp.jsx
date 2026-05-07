f6504a4acd2f9334bad2bbce5729680c568a1f05
dikshapathak15 <dikshapathak1541@gmail.com>
2026-05-07 11:00:55 +0530

    signup with password

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,18 +1,86 @@
-import React from 'react'
+import React, { useState } from "react";
+import { FaEye } from "react-icons/fa6";
+import { FaEyeSlash } from "react-icons/fa6";
 
-function SignUp () {
-    const primaryColor = "#ff4d2d";
-    const hoverColor = "#e64323";
-    const bgColor = "#fff9f6";
-    const borderColor = "#ddd";
+function SignUp() {
+  const primaryColor = "#ff4d2d";
+  const hoverColor = "#e64323";
+  const bgColor = "#fff9f6";
+  const borderColor = "#ddd";
+
+  const [showPassword, setShowPassword] = useState(false)
+
+  const [role, setRole] = useState("user")
 
   return (
-    <div className='min-h-screen w-full flex items-center justify-center p-4' style={{background:bgColor}}>
-        <div className='bg-white rounded'>
+    <div
+      className="min-h-screen w-full flex items-center justify-center p-4"
+      style={{ background: bgColor }}
+    >
+      <div
+        className={
+          "bg-white rounded-xl shadow-lg w-full max-w-md p-8 border-[1px]"
+        }
+        style={{ border: `1px solid ${borderColor}` }}
+      >
+        <h1
+          className={`text-3xl font-bold mb-2`}
+          style={{ color: primaryColor }}
+        >
+          Vingo
+        </h1>
+        <p>Create your account to get started with delicious food deliveries</p>
 
+        {/* {  fullName } */}
+        <div className="mb-4">
+          <label htmlFor="fullName" className="block text-gray-700 font-medium mb-1">Full Name</label>
+          <input type="text" className="w-full border rounded-lg px-3 py-2 focus:outline-none " placeholder="Enter your full name" 
+          style={{border:`1px solid ${borderColor}`}}/>
         </div>
+
+        {/* {  email } */}
+        <div className="mb-4">
+          <label htmlFor="email" className="block text-gray-700 font-medium mb-1">Email</label>
+          <input type="email" className="w-full border rounded-lg px-3 py-2 focus:outline-none " placeholder="Enter your email" 
+          style={{border:`1px solid ${borderColor}`}}/>
+        </div>
+
+        {/* {  mobile } */}
+        <div className="mb-4">
+          <label htmlFor="mobile" className="block text-gray-700 font-medium mb-1">Mobile</label>
+          <input type="text" className="w-full border rounded-lg px-3 py-2 focus:outline-none " placeholder="Enter your mobile no" 
+          style={{border:`1px solid ${borderColor}`}}/>
+        </div>
+
+        {/* {  password } */}
+        <div className="mb-4">
+          <label htmlFor="password" className="block text-gray-700 font-medium mb-1">Password</label>
+          <div className="relative">
+          <input type={`${showPassword? "text" : "password"}`} className="w-full border rounded-lg px-3 py-2 focus:outline-none " placeholder="Enter your Password" 
+          style={{border:`1px solid ${borderColor}`}}/>
+          <button className="absolute right-3 top-[14px] text-gray-500" onClick={()=>setShowPassword(prev => !prev)}>{!showPassword ? <FaEye /> : <FaEyeSlash />}</button>
+          </div>
+        </div>
+
+
+         {/* {  role } */}
+        <div className="mb-4">
+          <label htmlFor="role" className="block text-gray-700 font-medium mb-1">Role</label>
+          <div className="flex gap-2">
+          {["user", "owner", "deliveryBoy"].map((r)=>(
+            <button className="flex-1 border rounded-lg px-3 py-2 text-center font-medium transition-colors cursor-pointer" 
+            onClick={()=> setRole(r)}
+            style={
+              role == r ? {backgroundColor: primaryColor,color:"white"} : {border:`1px solid ${primaryColor}`, color :primaryColor}
+            }>{r}</button>
+          ))}
+          </div>
+        </div>
+        <button className={`w-full mt-4 flex items-center justify-center gap-2 border rounded-lg px-4 py-2 transition duration-200 bg-[#ff4d2d] text-white hover:bg-[#e64323] cursor-pointer`}>Sign Up</button>
+
+      </div>
     </div>
-  )
+  );
 }
 
-export default SignUp
+export default SignUp;
