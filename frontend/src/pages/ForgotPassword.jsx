d2a7b9aeecd0c0a2477aedc6a14bf63d96528c3f
dikshapathak15 <dikshapathak1541@gmail.com>
2026-06-10 17:28:00 +0530

    step 2 completed

--- frontend/src/pages/ForgotPassword.jsx
+++ frontend/src/pages/ForgotPassword.jsx
@@ -3,8 +3,9 @@ import { FaArrowLeftLong } from "react-icons/fa6";
 import { useNavigate } from "react-router-dom";
 
 const ForgotPassword = () => {
-  const [step, setStep] = useState(1);
+  const [step, setStep] = useState(2);
   const [email, setEmail] = useState();
+  const [otp, setOTP] = useState("");
   const navigate = useNavigate()
   return (
     <div className="flex w-full items-center justify-center min-h-screen p-4 bg-[#fff9f6]">
@@ -41,6 +42,61 @@ const ForgotPassword = () => {
             </button>
           </div>
         )}
+
+        {step === 2 && (
+          <div>
+            {" "}
+            <div className="mb-4">
+              <label
+                htmlFor="email"
+                className="block text-gray-700 font-medium mb-1"
+              >
+                OTP
+              </label>
+              <input
+                type="email"
+                className="w-full border-[1px] rounded-lg border-gray-200 px-3 py-2 focus:outline-none "
+                placeholder="Enter your OTP"
+                onChange={(e) => setOtp(e.target.value)}
+                value={otp}
+              />
+            </div>
+            <button
+              className={`w-full mt-4 flex items-center justify-center gap-2 border rounded-lg px-4 py-2 transition duration-200 bg-[#ff4d2d] text-white hover:bg-[#e64323] cursor-pointer`}
+              
+            >
+              Verify
+            </button>
+          </div>
+        )}  
+
+        {step === 3 && (
+          <div>
+            {" "}
+            <div className="mb-4">
+              <label
+                htmlFor="email"
+                className="block text-gray-700 font-medium mb-1"
+              >
+                OTP
+              </label>
+              <input
+                type="email"
+                className="w-full border-[1px] rounded-lg border-gray-200 px-3 py-2 focus:outline-none "
+                placeholder="Enter your OTP"
+                onChange={(e) => setOtp(e.target.value)}
+                value={otp}
+              />
+            </div>
+            <button
+              className={`w-full mt-4 flex items-center justify-center gap-2 border rounded-lg px-4 py-2 transition duration-200 bg-[#ff4d2d] text-white hover:bg-[#e64323] cursor-pointer`}
+              
+            >
+              Verify
+            </button>
+          </div>
+        )}
+        
       </div>
     </div>
   );
