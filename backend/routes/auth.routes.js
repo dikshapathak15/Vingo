85328def0a1d4029f523c171cfe5133f887a0c93
dikshapathak15 <dikshapathak1541@gmail.com>
2026-06-11 21:37:07 +0530

    routes

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,11 +1,14 @@
 import express from "express"
-import { signIn, signUp, signOut } from "../controller/auth.controller.js"
+import { signIn, signUp, signOut, sendOtp, verifyOtp, resetPassword } from "../controller/auth.controller.js"
 
 const authRouter = express.Router()
 
 authRouter.post("/signup" , signUp)
 authRouter.post("/signin" , signIn)
 authRouter.get("/signout" , signOut)
+authRouter.post("/send-otp" , sendOtp)
+authRouter.post("/verify-otp" , verifyOtp)
+authRouter.post("/reset-password" , resetPassword)
 
 
 export default authRouter
