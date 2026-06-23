ed6ec6d890ad853b61ff9d32751a50ef88e6a373
dikshapathak15 <dikshapathak1541@gmail.com>
2026-06-23 14:22:50 +0530

    google auth controller

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -139,3 +139,26 @@ export const resetPassword = async(req,res)=>{
      return res.status(500).json(`reset password error ${error}`);
   }
 }
+
+
+export const googleAuth = async(req,res) =>{
+  try {
+    const {fullName, email, mobile} = req.body
+    let user = await User.findOne({email})
+    if(!user){
+      user = await User.create({
+        fullName,email, mobile
+      })
+    }
+     const token = await genToken(user._id);
+    res.cookie("token", token, {
+      secure: false,
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      httpOnly: true,
+    });
+
+  } catch (error) {
+     return res.status(500).json(`google auth error ${error}`);
+  }
+}
