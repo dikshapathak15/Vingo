12ceeefabebaaa64058b792efa51f04450ffd506
dikshapathak15 <dikshapathak1541@gmail.com>
2026-06-24 12:41:02 +0530

    decodeToken to get current user

---
+++ backend/middleware/isAuth.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken"
+
+const isAuth = async(req,res, next) =>{
+    try {
+        const token = req.cookies.token
+        if(!token){
+            response.status(400).json({message : "token not found"})
+        }
+        const decodeToken = jwt.verify(token,process.env.JWT_SECRET)
+        if(!decodeToken){
+              response.status(400).json({message : "TOKEN NOT VERIFIED"})
+        }
+        console.log(decodeToken)
+        req.userId = decodeToken.userId
+    } catch (error) {
+          response.status(500).json({message : "isAuth error"})
+    }
+}
