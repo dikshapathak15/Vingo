c815b734d81af82fc8c94f0d2e6b3c4c15da4c7f
dikshapathak15 <dikshapathak1541@gmail.com>
2026-06-25 09:54:31 +0530

    created homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,10 +4,12 @@ import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
 import ForgotPassword from './pages/ForgotPassword'
 import useGetCurrentUser from './hooks/getCurrentUser'
+import { useSelector } from 'react-redux'
 export const serverUrl = "http://localhost:8000"
 
 function App() {
   useGetCurrentUser()
+  const {userData} = useSelector(state=>state.user)
   return (
     <Routes>
       <Route path ='/signup' element = {<SignUp/>}/>
