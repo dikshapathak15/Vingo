3686d3c73db16d7e30f8c8990c7515d7a2ae445a
dikshapathak15 <dikshapathak1541@gmail.com>
2026-07-07 16:13:19 +0530

    responsive myorders

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -12,20 +12,29 @@ function Nav() {
       <h1 className="text-3xl font-bold mb-2 text-[#ff4d2d]">
         Vingo
       </h1>
-      <div className="w-[60%] md:w-[60%] lg:w-[40%] h-[70px] bg-white shadow-xl rounded-lg items-center gap-[20px] flex">
+      <div className="w-[60%] md:w-[60%] lg:w-[40%] h-[70px] bg-white shadow-xl rounded-lg items-center gap-[20px] hidden md:flex">
         <div className="flex items-center w-[30%] overflow-hidden gap-[10px] px-[10px] border-r-[2px] border-gray-400">
             <FaLocationDot size={25} className="text-[#ff4d2d]" />
         <div className="w-[80%] truncate text-gray-600 ">jhansi</div></div>
-        <div className="w-[80%] flex items-center gap-[10px]"><IoIosSearch size={25} className="text-[#ff4d2d]"/>
+        <div className="w-[80%] flex items-center gap-[10px]"><IoIosSearch size={25} className="text-[#ff4d2d] "/>
         <input type="text" placeholder='search delicious food....' className="px-[10px] text-gray-700 ouline-0 w-full" />
         </div>
       </div>
+      <div className="flex items-center gap-4">
+        <IoIosSearch size={25} className="text-[#ff4d2d] md:hidden"/>
       <div className="relative cursor-pointer">
         <FiShoppingCart size={25} className="text-[#ff4d2d]" />
         <span className="absolute right-[-9px] top-[-12px] text-[#ff4d2d] font-medium">0</span>
       </div>
       <button className="hidden md:block px-3 py-1 rounded-lg bg-[#ff4d2d]/10 text-[#ff4d2d] text-sm font-medium ">My Orders</button>
       <div className="w-[40px] h-[40px] rounded-full flex items-center justify-center bg-[#ff4d2d] text-white text-[18px] shadow-xl font-semibold cursor-pointer">{userData?.fullName.slice(0,1)}</div>
+    <div className="fixed top-[80px] right-[10px] md:right-[10%] lg:right-[25%] w-[180px] bg-white shadow-2xl rounded-xl p-[20px] flex flex-col gap-[10px] z-[9999]">
+      <div className="text-[17px] font-semibold">{userData.fullName}</div>
+      <div className="md:hidden text-[#ff4d2d] font-semibold cursor-pointer">My Orders</div>
+      <div className="text-[#ff4d2d] font-semibold cursor-pointer">Log Out</div>
+    </div>
+    
+    </div>
     </div>
   );
 }
