d3eb2c915f2964608e0b1042ce9b1454f739c2cf
dikshapathak15 <dikshapathak1541@gmail.com>
2026-07-08 17:49:11 +0530

    item and shop model created

---
+++ backend/models/shop.model.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+import mongoode from "mongoose";
+
+const shopSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    state: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
