379ad1504f993d8b75c96e4d1ec7c9a552d5677c
dikshapathak15 <dikshapathak1541@gmail.com>
2026-07-08 17:10:36 +0530

    getting the current city through geoApi

--- frontend/src/hooks/useGetCity.jsx
+++ frontend/src/hooks/useGetCity.jsx
@@ -6,9 +6,14 @@ import { setUserData } from "../redux/userSlice";
 
 function useGetCity() {
   const dispatch = useDispatch();
+  const apiKey = import.meta.env.VITE_GEOAPIKEY
  useEffect(() => {
 navigator.geolocation.getCurrentPosition(async(position)=> {
 console.log(position);
+const latitude = position.coords.latitude;
+const longitude = position.coords.longitude;
+const result = await axios.get(`https://api.geoapify.com/v1/geocode/reverse?lat=${latitude}&lon=${longitude}&format=json&apiKey=${apiKey}`)
+console.log(result.data.results[0].city);
 })
  },[])
 }
