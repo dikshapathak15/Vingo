2d3900c6dc43f917538ac94a87cf4b230dea2449
dikshapathak15 <dikshapathak1541@gmail.com>
2026-07-08 18:15:19 +0530

    cloudinary setUp

--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -1,1 +1,20 @@
-import { v2 as cloudinary } from 'cloudinary'
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs"
+
+const uploadOnCloudinary = async (file) => {
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_APIKEY,
+    api_secret: process.env.CLOUDINARY_APISECRET,
+  });
+  try {
+    const result = await cloudinary.uploader.upload(file);
+    fs.unlinkSync(file);
+    return result.secure_url;
+  } catch (error) {
+     fs.unlinkSync(file);
+     console.log(error);
+  }
+};
+
+export default uploadOnCloudinary;
