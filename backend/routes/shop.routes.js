ea09132f7ab865103384bc4829a0fd1fe8410616
dikshapathak15 <dikshapathak1541@gmail.com>
2026-08-03 18:44:43 +0530

    get my shop routes

--- backend/routes/shop.routes.js
+++ backend/routes/shop.routes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { createEditShop } from "../controller/shop.controllers"
+import { createEditShop, getMyShop } from "../controller/shop.controllers"
 import isAuth from "../middleware/isAuth"
 import { upload } from "../middleware/multer"
 
@@ -7,7 +7,8 @@ import { upload } from "../middleware/multer"
 
 const shopRouter = express.Router()
 
-shopRouter.get("/create-edit" ,isAuth,upload.single("image"), createEditShop)
+shopRouter.post("/create-edit" ,isAuth,upload.single("image"), createEditShop)
+shopRouter.get("/get-my" ,isAuth,getMyShop)
 
 
 
