2256982c3490638c8952418010e5c3be5ad0c782
dikshapathak15 <dikshapathak1541@gmail.com>
2026-08-03 18:06:50 +0530

    navbar for ownerDashboard

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -24,7 +24,7 @@ function Nav() {
   }
   return (
     <div className="w-full h-[80px] flex items-center justify-center md:justify-center gap-[30px] px-[20px] fixed top-0 left-0 z-[9999] bg-[#fff9f6] overflow-visible">
-      {showSearch && (
+      {showSearch && userData.role == "user" && (
         <div className="w-[90%] h-[70px]  bg-white shadow-xl rounded-lg items-center gap-[20px] flex md:flex fixed top-[80px] left-[5%] md:hidden">
           <div className="flex items-center w-[30%] overflow-hidden gap-[10px] px-[10px] border-r-[2px] border-gray-400">
             <FaLocationDot size={25} className="text-[#ff4d2d]" />
@@ -41,6 +41,7 @@ function Nav() {
         </div>
       )}
       <h1 className="text-3xl font-bold mb-2 text-[#ff4d2d]">Vingo</h1>
+      {userData.role == "user" && 
       <div className="w-[60%] md:w-[60%] lg:w-[40%] h-[70px] bg-white shadow-xl rounded-lg items-center gap-[20px] hidden md:flex">
         <div className="flex items-center w-[30%] overflow-hidden gap-[10px] px-[10px] border-r-[2px] border-gray-400">
           <FaLocationDot size={25} className="text-[#ff4d2d]" />
@@ -55,8 +56,9 @@ function Nav() {
           />
         </div>
       </div>
+}
       <div className="flex items-center gap-4">
-        {showSearch?<ImCross size={20} className="text-[#ff4d2d] md:hidden" onClick={()=> setShowSearch(false)} />:  <IoIosSearch size={25} className="text-[#ff4d2d] md:hidden" onClick={()=> setShowSearch(true)} />}
+        {userData.role == "user" && (showSearch?<ImCross size={20} className="text-[#ff4d2d] md:hidden" onClick={()=> setShowSearch(false)} />:  <IoIosSearch size={25} className="text-[#ff4d2d] md:hidden" onClick={()=> setShowSearch(true)} />)}
        
         <div className="relative cursor-pointer">
           <FiShoppingCart size={25} className="text-[#ff4d2d]" />
--- frontend/src/components/ownerDashboard.jsx
+++ frontend/src/components/ownerDashboard.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Nav from './Nav'
 
 const ownerDashboard = () => {
   return (
-    <div></div>
+    <div><Nav>
+      </Nav></div>
   )
 }
 
