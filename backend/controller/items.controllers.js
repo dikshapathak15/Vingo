5855705ca4623c0599833a996a4b27cbf8c059c3
dikshapathak15 <dikshapathak1541@gmail.com>
2026-08-03 17:24:50 +0530

    add item controller

---
+++ backend/controller/items.controllers.js
@@ -0,0 +1,26 @@
+import uploadOnCloudinary from "../utils/cloudinary";
+
+export const addItem = async (req,res)=>{
+    try {
+        const {name,category,foodType,price} = req.body;
+        let image;
+        if(req.file){
+            image = await uploadOnCloudinary(req.file.path)
+        }
+        const shop = await Shop.findOne({owner:req.userId})
+        if(!shop){
+            return req.status(400).json({message:"shop not found"})
+        }
+        const item = await Item.create({
+            name,category,foodType,price,image,shop:shop._id
+        })
+
+            return req.status(201).json(item)
+    } catch (error) {
+        return req.status(500).json({message:`add item error ${error}`})
+    }
+}
+
+export const editItem = async (req,res)=>{
+    
+}
