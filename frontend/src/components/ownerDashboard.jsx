1df15a846b8691b0174c7995913334d27c32dc3a
dikshapathak15 <dikshapathak1541@gmail.com>
2026-08-10 19:37:05 +0530

    initiated ownerDashboard

--- frontend/src/components/ownerDashboard.jsx
+++ frontend/src/components/ownerDashboard.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import Nav from './Nav'
+import { useSelector } from 'react-redux'
 
 const ownerDashboard = () => {
+  const {myShopData} = useSelector(state=>state.owner)
   return (
-    <div><Nav>
-      </Nav></div>
+    <div className='w-full min-h-screen bg-[#fff9f6] flex flex-col items-center'><Nav>
+      </Nav>
+      
+      {!myShopData &&  
+      
+      <div className='flex justify-center items-center p-4 sm:p-6'>
+        <div className='w-full max-w-md bg-white shadow-lg rounded-2xl p-6 border border-gray-100 hover:shadow-xl transition-shadow duration-300'>
+          <div className='flex flex-col items-center text-center'>
+
+          </div>
+        </div>
+      </div>
+      
+      }
+
+      </div>
   )
 }
 
