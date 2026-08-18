8f769a93500cf171882a533316058ce44055f371
dikshapathak15 <dikshapathak1541@gmail.com>
2026-08-18 23:17:07 +0530

    initiated form to add shop

--- frontend/src/pages/CreateEditShop.jsx
+++ frontend/src/pages/CreateEditShop.jsx
@@ -24,6 +24,12 @@ function CreateEditShop() {
           </div>
           <div className="text-3xl font-extrabold text-gray-900">{myShopData ? "Edit Shop" : "Add Shop"}</div>
         </div>
+        <form>
+          <div>
+            <label className="block text-sm font-medium text-gray-700 mb-1">Name</label>
+            <input type="text" placeholder="Enter Shop Name" className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500" />
+          </div>
+        </form>
       </div>
     </div>
   );
