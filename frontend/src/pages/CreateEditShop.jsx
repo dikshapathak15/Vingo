4198a7f493519763bdb4209e72e53e53117f633d
dikshapathak15 <dikshapathak1541@gmail.com>
2026-08-18 23:11:48 +0530

    add shop

--- frontend/src/pages/CreateEditShop.jsx
+++ frontend/src/pages/CreateEditShop.jsx
@@ -1,26 +1,29 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { IoArrowBackOutline } from "react-icons/io5";
+import { FaUtensils } from "react-icons/fa";
+import { useSelector } from "react-redux";
 
 function CreateEditShop() {
   const navigate = useNavigate();
-  const {myShopData} = useSelector(state=>state.owner)
+  const { myShopData } = useSelector((state) => state.owner);
 
   return (
-    <div className="felx justify-center flex-col items-center p-6 bg-gradient-to-br from-orange-50 relative to-white min-h-screen">
+    <div className="flex justify-center flex-col items-center p-6 bg-gradient-to-br from-orange-50 relative to-white min-h-screen">
       <div
         className="absolute top-[20px] left-[20px] z-[10] mb-[10px]"
-        onClick={()=>navigate("/")}
+        onClick={() => navigate("/")}
       >
-        <IoArrowBackOutline size={35} className='text-[#ff4d2d]'/>
+        <IoArrowBackOutline size={35} className="text-[#ff4d2d]" />
       </div>
 
-      <div className="max-w-lg w-full bg-white shadow-xl rounded-2xl p-8 border border-orange-100">
-        <div className="">
-           <div></div>
-           <div></div>
+      <div className=" max-w-lg w-full bg-white shadow-xl rounded-2xl p-8 border border-orange-100">
+        <div className="flex flex-col items-center mb-6">
+          <div className="bg-orange-100 p-4 rounded-full mb-4">
+            <FaUtensils className="text-[#ff4d2d] w-16 h-16" />
+          </div>
+          <div className="text-3xl font-extrabold text-gray-900">{myShopData ? "Edit Shop" : "Add Shop"}</div>
         </div>
-
       </div>
     </div>
   );
